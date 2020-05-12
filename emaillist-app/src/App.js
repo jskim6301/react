59202f61b4e993331d172b7a76041cfcccf1e167
jskim <jskim@naver.com>
2020-05-12 09:03:43 +0900

    life cycle API 통신 데이터

---
+++ emaillist-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EmaillistApp from './EmaillistApp';
+
+export default class App extends React.Component {
+   render(){
+      return (
+         <EmaillistApp />
+            
+      );
+   }
+}
---
+++ emaillist-app/src/EmaillistItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class EmaillistItem extends React.Component {
+   render(){
+      return (
+         <div className='EmaillistItem'>
+            <li>
+                {this.props.name}
+                <br/>
+                {this.props.email}
+            </li>
+         </div>
+      );
+   }
+}
